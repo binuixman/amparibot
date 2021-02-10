1543ae3551d95cfcc57e4555c07949e2a36a7521
binuixman <75389229+binuixman@users.noreply.github.com>
2021-02-10 20:44:15 +0200

    Made bot to query for games in set intervals.

--- bot_startup.py
+++ bot_startup.py
@@ -3,6 +3,7 @@
 from irc_class import *
 import os
 import random
+import time
 from cheapshark_query import *
 
 ## IRC Config
@@ -14,10 +15,17 @@ irc = IRC()
 irc.connect(server, port, channel, botnick)
 
 freegames = get_free_games()
+most_recent = time.time()
+interval = 3600  # Query interval in seconds
 
 while True:
     text = irc.get_response()
     print(text)
+    
+    curr = time.time()
+    if curr - most_recent > interval:
+        freegames = get_free_games()
+        most_recent = curr
 
     if "PRIVMSG" in text and channel in text and "ämpäri?" in text:
         header = ":Tämän päivän ämpärit: "
